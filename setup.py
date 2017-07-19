a7cea878bf077ce510c627c1120873d4b3054a15
Frederick-S <mao_xiaodan@hotmail.com>
2017-07-19 12:15:40 +0800

    Package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='is-taken',
+    version='0.1',
+    scripts=['is_takey.py'],
+    description='Check if a PyPI package name is taken.',
+    url='https://github.com/Frederick-S/is-taken'
+)
